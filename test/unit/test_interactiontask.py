fbbc0834d01c3d184c3c3f18ce9e5a616e9f7a70
Christoph Johns <christophjohns@gmail.com>
2021-12-13 15:25:17 +0200

    Add test for empty init

--- test/unit/test_interactiontask.py
+++ test/unit/test_interactiontask.py
@@ -11,13 +11,30 @@ def test_imports():
 def test_example():
     """Tries to import and create the example task."""
     from coopihc import ExampleTask
+
     ExampleTask()
+
+
+def test_init():
+    """Tries to initialize an InteractionTask and checks the expected
+    properties and methods."""
+    assert empty_init_fails()
+
+
+def empty_init_fails():
+    """Returns True if trying to initialize an InteractionTask
+    without any arguments fails."""
+    try:
+        InteractionTask()
+    except TypeError:
+        return True
 
 
 def test_interactiontask():
     """Tests the methods provided by the InteractionTask class."""
     test_imports()
     test_example()
+    test_init()
 
 
 # +----------------------+
