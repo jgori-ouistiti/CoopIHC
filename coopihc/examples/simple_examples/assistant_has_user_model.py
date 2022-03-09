e86048d1a27ae4d481b84c253656b34f31b62a47
jgori <juliengori@gmail.com>
2022-03-09 14:13:41 +0100

    trying out some stuff for rollouts

--- coopihc/examples/simple_examples/assistant_has_user_model.py
+++ coopihc/examples/simple_examples/assistant_has_user_model.py
@@ -3,6 +3,7 @@ from coopihc.agents.ExampleUser import PseudoRandomUser, PseudoRandomUserWithPar
 from coopihc.agents.ExampleAssistant import (
     CoordinatedAssistant,
     CoordinatedAssistantWithInference,
+    CoordinatedAssistantWithRollout,
 )
 
 from coopihc.bundle.Bundle import Bundle
@@ -56,3 +57,20 @@ while True:
     if is_done:
         break
 # [end-user-model-inference]
+
+# # [start-user-model-rollout]
+# user = PseudoRandomUserWithParams(p=[1, 5, 7])
+# user_model = PseudoRandomUserWithParams(p=[5, 5, 7])  # Model mismatch
+
+# assistant = CoordinatedAssistantWithRollout(user_model=user_model)
+
+# bundle = Bundle(task=CoordinatedTask(), user=user, assistant=assistant)
+
+# bundle.reset(turn=2)
+# print(bundle.game_state)
+# exit()
+# while True:
+#     obs, rewards, is_done = bundle.step()
+#     if is_done:
+#         break
+# # [end-user-model-rollout]
